f9980c453f4ee5f1a150ded9a48613487ca2940a
uninicol <nicol.buratti@studenti.unicam.it>
2022-09-19 18:41:51 +0200

    viene richiesto prima i parametri di client e server

--- main.c
+++ main.c
@@ -1,31 +1,28 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <arpa/inet.h>
-#include <netdb.h>
 #include "server.h"
 #include "client.h"
 #include "host-address.h"
 
 int main(int argc, char *argv[]) {// definiamo come argomenti ip e porta (oppure lo facciamo automatico)
-    printf("\nHost address:\t%s\n", hostAddress());
-
-    char *ip;
+    printf("Host address:\t%s\n", hostAddress());
     int local_port;
-    int port;
-
-    printf("Inserire ip server:\t");
-    fgets(ip, 15, stdin);
-    printf("Inserire porta del server:\t");
-    scanf("%d", &port);
     printf("Inserire porta del server locale:\t");
     scanf("%d", &local_port);
-
 
     pid_t pid = fork();
     if (pid == 0)
         run_server(local_port);
-    else
+    else {
+        char *ip;
+        int port;
+
+        printf("Inserire ip server:\t");
+        scanf("%s", ip);
+        printf("Inserire porta del server:\t");
+        scanf("%d", &port);
         run_client(ip, port);
+    }
     return EXIT_SUCCESS;
 }
