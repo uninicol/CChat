b8c84165ccd128433cd1a0617ad057c37b3796dc
Nicol Buratti <nickibrt8@gmail.com>
2022-11-22 15:39:43 +0100

    eliminati commenti inutili

--- main.c
+++ main.c
@@ -5,19 +5,13 @@
 #include "client.h"
 #include "host-address.h"
 
-int main(int argc, char *argv[]) {// definiamo come argomenti ip e porta (oppure lo facciamo automatico)
+int main(int argc, char *argv[]) {
     printf("Host address:\t%s\n", hostAddress());
-    //TODO sia client che server possono scrivere
-    //TODO 1 arg= <porta dell'host>
-    //TODO 2 arg= <ip porta server>
-
     if (argc < 2 || argc > 3)
         perror("numero parametri non corretto");
     if (argc == 2) {
-        //server
         run_server(atoi(argv[1]));
     } else {
-        //client
         run_client(argv[1], atoi(argv[2]));
     }
     return EXIT_SUCCESS;
